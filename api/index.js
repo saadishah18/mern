981758a5dfbfbc87494df73c88cd4937b5870c33
saad <s.siddiquishah@gmail.com>
2023-10-01 02:30:30 +0500

    database changes

--- api/index.js
+++ api/index.js
@@ -1,5 +1,12 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose.connect(process.env.Mongo).then( () => {
+    console.log('monog connected');
+}).catch( (err) => {
+    console.error(err);
+} );
 const app = express();
 
 app.listen(3000, () => {
