762c0bfdce6d852bdddc1fb027030805fc6a8b05
saad <s.siddiquishah@gmail.com>
2023-10-06 01:10:18 +0500

    sign in api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import User from "../models/user.model.js";
 import bycryptjs from 'bcryptjs';
 import { errorHandler } from "../Utils/error.js";
+import jwt from 'jsonwebtoken';
+
+
 export const signup = async (req, res, next) => {
     const {username, email, password} = req.body;
     const hashPassword = bycryptjs.hashSync(password,10);
@@ -17,3 +20,24 @@ export const signup = async (req, res, next) => {
     }
    
 };
+
+
+export const signin = async (req, res, next) => {
+    const {email, password} = req.body;
+    try{
+        const validUser = await User.findOne({email});
+        if(!validUser) return next(errorHandler(404, 'User Not found'));
+        const validPassword = bycryptjs.compareSync(password, validUser.password);
+        if(!validPassword) return next(errorHandler(401, 'Credentials do not matched'));
+        let id = validUser._id;
+       
+        const token = jwt.sign({id }, process.env.JWT_SECRET);
+        // hid epassword from response;
+        const {password: pass, ...rest} = validUser._doc;
+        res.cookie('access_token' , token, {
+            httpOnly: true, 
+        }).status(200).json(rest);
+    }catch(error){
+        next(error);
+    }
+}
